13a4280a30da4b05355fd8846b6bdb99b71a4e8c
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-05-15 13:10:36 -0400

    Refactor to an Express app

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
-const http = require("http");
-const fs = require("fs");
+//Replace with Express app:
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  if (req.url === "/") {
-    fs.readFile("./index.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/about") {
-    fs.readFile("./about.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/contact") {
-    fs.readFile("./contact.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else {
-    fs.readFile("./404.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(400, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  }
+//Listen for requests:
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
 
-server.listen(8080);
+// const http = require("http");
+// const fs = require("fs");
+
+app.get("/", (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./about.html", { root: __dirname });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile("./contact.html", { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./404.html", { root: __dirname });
+});
+
+// const server = http.createServer((req, res) => {
+
+//   if (req.url === "/") {
+//     fs.readFile("./index.html", (err, data) => {
+//       if (err) throw err;
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.end(data);
+//     });
+//   } else if (req.url === "/about") {
+//     fs.readFile("./about.html", (err, data) => {
+//       if (err) throw err;
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.end(data);
+//     });
+//   } else if (req.url === "/contact") {
+//     fs.readFile("./contact.html", (err, data) => {
+//       if (err) throw err;
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.end(data);
+//     });
+//   } else {
+//     fs.readFile("./404.html", (err, data) => {
+//       if (err) throw err;
+//       res.writeHead(400, { "Content-Type": "text/html" });
+//       res.end(data);
+//     });
+//   }
+// });
+
+// server.listen(8080);
